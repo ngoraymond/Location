ce84dd4d5a7a2f4ae9ee32a6fa984b8b26b41990
ngoraymond <21043327+ngoraymond@users.noreply.github.com>
2019-02-09 11:55:34 -0500

    s

--- geoloc.js
+++ geoloc.js
@@ -1,10 +1,10 @@
 var map;
-       function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 0, lng: 0},
-          zoom: 8
-        });
-    }
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 0, lng: 0},
+    zoom: 8
+    });
+}
 function findMe() {
     var output = document.getElementById("out");
 
