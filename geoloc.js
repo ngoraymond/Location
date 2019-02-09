72ba049e10d3f9a14f21662672f396a4d865feb9
ngoraymond <21043327+ngoraymond@users.noreply.github.com>
2019-02-09 11:33:29 -0500

    d

--- geoloc.js
+++ geoloc.js
@@ -6,24 +6,22 @@ function findMe() {
         return;
     }
 
-    function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: latitude, lng: longitude},
-          zoom: 8
-        });
-
     function success(position) {
         var latitude  = position.coords.latitude;
         var longitude = position.coords.longitude;
 
         output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
-        initMap();
+        
         /*
         var img = new Image();
         img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false&key=AIzaSyCLWUexYVG_0sCUdycc6zIsNJjUGiTcz7k";
 
         output.appendChild(img);
         */
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: latitude, lng: longitude},
+          zoom: 8
+        });
     }
 
     function error() {
